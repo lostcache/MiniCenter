8aa53a8302111c1d6c5e355d2a34475be425cfa8
lostcache <harshdevisha9111@gmail.com>
2025-05-17 10:31:54 +0000

    vanilla l2 learning switch

--- simple_controller.py
+++ simple_controller.py
@@ -1,187 +1,110 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+An OpenFlow 1.0 L2 learning switch implementation.
+"""
+
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
 
-class SimpleController(app_manager.RyuApp):
-    """
-    A simple controller for Fat Tree topologies.
-
-    This controller implements a basic learning switch that will enable
-    connectivity between hosts in the Fat Tree topology.
-    """
-
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+class SimpleSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleController, self).__init__(*args, **kwargs)
-        # MAC address table maps MAC addresses to switch ports
+        super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self.logger.info("Simple Controller Started")
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):
-        """
-        Handle switch connection and install table-miss flow entry.
-
-        A table-miss flow entry sends all unmatched packets to the controller.
-        """
-        datapath = ev.msg.datapath
+    def add_flow(self, datapath, in_port, dst, src, actions):
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
 
-        # Clear flow table on switch connection
-        self.clear_flows(datapath)
+        match = datapath.ofproto_parser.OFPMatch(
+            in_port=in_port,
+            dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src))
 
-        # Install table-miss flow entry
-        match = parser.OFPMatch()
-        actions = [
-            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
-        ]
-        self.add_flow(datapath, 0, match, actions)
-        self.logger.info(f"Switch {datapath.id} connected")
-
-    def clear_flows(self, datapath):
-        """
-        Clear all flows from the switch's flow table.
-        """
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        # Create a flow mod message to delete all flows
-        match = parser.OFPMatch()
-        instructions = []
-        flow_mod = parser.OFPFlowMod(
-            datapath=datapath,
-            command=ofproto.OFPFC_DELETE,
-            out_port=ofproto.OFPP_ANY,
-            out_group=ofproto.OFPG_ANY,
-            match=match,
-            instructions=instructions,
-        )
-        datapath.send_msg(flow_mod)
-
-    def add_flow(
-        self, datapath, priority, match, actions, buffer_id=None, hard_timeout=0
-    ):
-        """
-        Install a flow entry to the switch's flow table.
-        """
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(
-                datapath=datapath,
-                buffer_id=buffer_id,
-                priority=priority,
-                match=match,
-                instructions=inst,
-                hard_timeout=hard_timeout,
-            )
-        else:
-            mod = parser.OFPFlowMod(
-                datapath=datapath,
-                priority=priority,
-                match=match,
-                instructions=inst,
-                hard_timeout=hard_timeout,
-            )
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=ofproto.OFP_DEFAULT_PRIORITY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        """
-        Handle packet-in events from switches.
-
-        This is the main logic for the learning switch:
-        1. Learn the source MAC address and port
-        2. If the destination is known, forward to the right port
-        3. If unknown, flood the packet
-        """
-        # If you hit this, you might want to increase the "miss_send_length" of your switch
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug(
-                "Packet truncated: only %s of %s bytes",
-                ev.msg.msg_len,
-                ev.msg.total_len,
-            )
-
+    def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match["in_port"]
 
-        # Get datapath ID to identify the switch
-        dpid = datapath.id
-
-        # Initialize mac_to_port table for this switch if not present
-        if dpid not in self.mac_to_port:
-            self.mac_to_port[dpid] = {}
-
-        # Parse the packet
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        # Ignore LLDP packets
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
             return
-
-        # Ignore IPv6 neighbor discovery packets
-        if eth.ethertype == ether_types.ETH_TYPE_IPV6:
-            return
-
-        # Get MAC addresses
         dst = eth.dst
         src = eth.src
 
-        # Learn MAC address to avoid FLOOD next time
-        self.mac_to_port[dpid][src] = in_port
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
 
-        # Log packet information
-        self.logger.info(
-            f"Packet in switch:{dpid} src:{src} dst:{dst} in_port:{in_port}"
-        )
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
-        # Determine output port
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = msg.in_port
+
         if dst in self.mac_to_port[dpid]:
-            # Known destination, forward to the right port
             out_port = self.mac_to_port[dpid][dst]
         else:
-            # Unknown destination, flood the packet
             out_port = ofproto.OFPP_FLOOD
 
-        # Construct action list
-        actions = [parser.OFPActionOutput(out_port)]
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
-        # Install a flow to avoid packet-in next time
+        # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # Install with higher priority and a hard timeout
-            self.add_flow(datapath, 1, match, actions, hard_timeout=300)
+            self.add_flow(datapath, msg.in_port, dst, src, actions)
 
-        # Send packet out to handle the current packet
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=msg.buffer_id,
-            in_port=in_port,
-            actions=actions,
-            data=data,
-        )
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data=data)
         datapath.send_msg(out)
 
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def _port_status_handler(self, ev):
+        msg = ev.msg
+        reason = msg.reason
+        port_no = msg.desc.port_no
 
-if __name__ == "__main__":
-    print("Run this controller with ryu-manager:")
-    print("ryu-manager simple_controller.py")
+        ofproto = msg.datapath.ofproto
+        if reason == ofproto.OFPPR_ADD:
+            self.logger.info("port added %s", port_no)
+        elif reason == ofproto.OFPPR_DELETE:
+            self.logger.info("port deleted %s", port_no)
+        elif reason == ofproto.OFPPR_MODIFY:
+            self.logger.info("port modified %s", port_no)
+        else:
+            self.logger.info("Illeagal port state %s %s", port_no, reason)
